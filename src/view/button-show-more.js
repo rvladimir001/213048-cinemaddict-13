7c5da9b206751cf64039c0ae31c5f158ab7871a4
rvladimir001 <rvladimir001@gmail.com>
2020-11-24 16:25:25 +0300

    скорректирован eslintrc, удалены лишние не используемые компоненты, а так же проведены корректировка по всем скриптам по ошибкам eslint

--- src/view/button-show-more.js
+++ src/view/button-show-more.js
@@ -1,3 +1,3 @@
-export const CreateButtonShowMore = ()=> {
-    return `<button class="films-list__show-more">Show more</button>`
-}
+export const createButtonShowMore = ()=> {
+  return `<button class="films-list__show-more">Show more</button>`;
+};
