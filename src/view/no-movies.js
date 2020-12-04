93215990505065a3bf4c3784e8c6b5d59b183a9e
rvladimir001 <rvladimir001@gmail.com>
2020-12-04 23:07:48 +0300

    добавлено событие на нажатие esc - закрытие окна с детализацией по фильму, добавлено отображение информиции об остуствии фильмов в базе по условию

---
+++ src/view/no-movies.js
@@ -0,0 +1,28 @@
+import {createElement} from "../utils";
+
+const createNoMoviesBlock = () => {
+  return (
+    `<h2 class="films-list__title">There are no movies in our database</h2>`
+  );
+};
+
+export default class NoMoviesBlock {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createNoMoviesBlock();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
