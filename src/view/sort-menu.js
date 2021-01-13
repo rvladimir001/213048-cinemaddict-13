f43d64887cc4cfd495a1311467bcdc38d452bc81
rvladimir001 <32569748+rvladimir001@users.noreply.github.com>
2021-01-13 13:55:59 +0300

    Update sort-menu.js

--- src/view/sort-menu.js
+++ src/view/sort-menu.js
@@ -3,15 +3,36 @@ import Abstract from "./abstract";
 export const createSortTemplate = () => {
   return (
     `<ul class="sort">
-      <li><a href="#" class="sort__button">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button sort__button--active">Sort by rating</a></li>
+      <li><a href="#" class="sort__button sort__button--active" data-sort="default">Sort by default</a></li>
+      <li><a href="#" class="sort__button" data-sort="date">Sort by date</a></li>
+      <li><a href="#" class="sort__button" data-sort="rating">Sort by rating</a></li>
     </ul>`
   );
 };
 
 export default class SortMenu extends Abstract {
+  constructor() {
+    super();
+    this._clickHandler = this._clickHandler.bind(this);
+  }
+
   getTemplate() {
     return createSortTemplate();
+  }
+
+  getActiveLink() {
+    return super.getElement().querySelector(`.sort__button--active`);
+  }
+
+  _clickHandler(evt) {
+    evt.preventDefault();
+    this._callback.click(evt);
+  }
+
+  setClickHandler(callback) {
+    this._callback.click = callback;
+    for (let botton of this.getElement().querySelectorAll(`.sort__button`)) {
+      botton.addEventListener(`click`, this._clickHandler);
+    }
   }
 }
