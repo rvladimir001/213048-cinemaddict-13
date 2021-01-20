b3605200011638b4ffa9282439edef0763870aaf
rvladimir001 <rvladimir001@gmail.com>
2021-01-20 12:56:44 +0300

    добавлен клсаа Smart

---
+++ src/view/smart.js
@@ -0,0 +1,24 @@
+import Abstract from "./abstract";
+
+export default class Smart extends Abstract {
+  constructor() {
+    super();
+    this._data = {};
+  }
+
+  updateData(date) {
+    this._data = Object.assign({}, this._data, date);
+    this.updateElement();
+  }
+
+  updateElement() {
+    const prevElement = this.getElement();
+    const parent = prevElement.parentElement;
+    this.removeElement();
+    const newElement = this.getElement();
+    parent.replaceChild(newElement, prevElement);
+    this.restoreHandlers();
+  }
+
+  restoreHandlers() {}
+}
