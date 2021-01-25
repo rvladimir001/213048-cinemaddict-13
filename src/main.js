015f1112cf9f8585cc269fc8b54e51eb8327d765
rvladimir001 <rvladimir001@gmail.com>
2021-01-25 16:55:23 +0300

    добавлена модель Films и класс observer

--- src/main.js
+++ src/main.js
@@ -5,19 +5,22 @@ import {render, RenderPosition} from "./utils/render.js";
 import {filmsSort} from "./utils/films.js";
 import MovieList from "./presenter/films-list-presenter.js";
 import {allFilmsForView} from "./mock/film";
+import Films from "./model/movies";
 
 const filmsSorted = filmsSort(allFilmsForView);
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = document.querySelector(`.header`);
 const siteFooterElement = document.querySelector(`.footer`);
 
+const filmsModel = new Films()
+filmsModel.setFilms(allFilmsForView)
 render(siteHeaderElement, new HeaderProfile(), RenderPosition.BEFOREEND);
 render(siteMainElement, new ListEmpty(filmsSorted.watchlist.length, filmsSorted.watched.length, filmsSorted.favorites.length), RenderPosition.BEFOREEND);
 
 const totalFilms = allFilmsForView.length;
-const filmsPresentor = new MovieList(siteMainElement, allFilmsForView);
+const filmsPresentor = new MovieList(siteMainElement, filmsModel);
 const siteFooterStat = siteFooterElement.querySelector(`.footer__statistics`);
 
 render(siteFooterStat, new FooterStat(totalFilms), RenderPosition.BEFOREEND);
 
-filmsPresentor.init();
+filmsPresentor.init(allFilmsForView);
